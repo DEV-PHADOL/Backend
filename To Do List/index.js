4035ac2eaed14313046d2e2b819bd623ae453380
DEV-PHADOL <devphadol1919@gmail.com>
2024-06-17 11:23:35 +0530

    read file

--- To Do List/index.js
+++ To Do List/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -8,7 +9,21 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.get('/',(req,res)=>{
-    res.render("index");
+    fs.readdir('./files',function(err,files){
+        res.render("index",{files: files});
+    })
+})
+
+app.get('/file/:filename',(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`, "utf-8" ,function(err,filedata){
+        res.render('show',{filename: req.params.filename ,filedata: filedata});
+    });
+})
+
+app.post('/create',(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,()=>{
+        res.redirect("/")
+    });
 })
 
 app.listen(3000,()=>{
